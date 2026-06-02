e7fee9aec52ec8ae4fe8fd426a82aadbfbc1dcb7
Ashish Shevale <shevaleashish@gmail.com>
2026-06-03 00:24:34 +0530

    Convert tabs to 2 spaces

--- init.lua
+++ init.lua
@@ -5,3 +5,7 @@ require("config.keymaps")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mousescroll = "ver:1,hor:1"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true

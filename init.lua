9c085a10b6729de6c8511e92c8e208fe4a322640
Ashish Shevale <shevaleashish@gmail.com>
2026-06-04 02:14:24 +0530

    Show entire error message inline in files

--- init.lua
+++ init.lua
@@ -9,3 +9,19 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 4,
+    source = "if_many",
+    prefix = "●",
+    format = function(diagnostic)
+      return string.format("%s", diagnostic.message)
+    end,
+  },
+  severity_sort = true,
+  float = {
+    border = "rounded",
+  },
+})
+
